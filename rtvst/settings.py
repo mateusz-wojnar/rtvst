d458d66b4bff2697e9457fe0282bb3cbc8c9df3f
mateusz-wojnar <mwojnar87@gmail.com>
2023-06-11 01:20:59 +0200

    mysql migrate

--- rtvst/settings.py
+++ rtvst/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'rtvst.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rtvst',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'donald08'
+
+
     }
 }
 
